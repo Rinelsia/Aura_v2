4462b4972359949cbb7d5e00892f327d7ea90874
Rinelsia <rinelsia@mail.ru>
2021-05-13 21:41:20 +0300

    Add files via upload

---
+++ Аура/Client/js/script.js
@@ -0,0 +1,38 @@
+"use strict"
+//
+
+function drag(ev){
+    console.log(ev);
+}
+function start(ev){
+    console.log(ev);
+}
+//
+function end(ev){
+    console.log(ev);
+}
+
+//Работа с ассинхронными запросами
+var ajax = new XMLHttpRequest();
+ajax.open('GET','http://localhost:1000/file.json', true);
+ajax.responseType = 'json';
+ajax.send();
+
+ajax.onload = function(){
+    
+    let d = ajax.response;
+    console.log('d'+d);
+}
+//let xhr = new XMLHttpRequest();
+//
+//xhr.open('GET', 'http://localhost:1000/');
+//
+//xhr.responseType = 'json';
+//
+//xhr.send();
+//
+//// тело ответа {"сообщение": "Привет, мир!"}
+//xhr.onload = function() {
+//  let responseObj = xhr.response;
+//  alert(responseObj.message); // Привет, мир!
+//};
